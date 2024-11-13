4ad882462bc4c3b248c3725a1df351ccc8ab712b
Ilya Zadorozhniy <xaxa-ilya@mail.ru>
2024-11-13 20:05:51 +0300

    update CsvReader class

--- lab4/src/main/java/org/example/CsvReader.java
+++ lab4/src/main/java/org/example/CsvReader.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+
 public class CsvReader {
+
+    public static List<Person> parsePersons(String sourcePath) throws IOException{
+        preprocessCSV(path);
+        return generateListOfPersons(getPath(sourcePath));
+    }
+
+    public static String getPath(){
+
+    }
+
+    private static List<Person> generateListOfPersons(String path) throws IOException{
+
+    }
+
+    private static void preprocessCSV(String path) throws  IOException{
+
+    }
+
+
 }
