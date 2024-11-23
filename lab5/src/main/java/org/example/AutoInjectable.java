bb6fb851b8dae63c25223b19cc425ae6d940116a
Ilya Zadorozhniy <xaxa-ilya@mail.ru>
2024-11-23 11:46:11 +0300

    done with AutoInjectable

--- lab5/src/main/java/org/example/AutoInjectable.java
+++ lab5/src/main/java/org/example/AutoInjectable.java
@@ -1,4 +1,10 @@
 package org.example;
 
-public class AutoInjectable {
+import java.lang.annotation.*;
+
+/** @AutoInjectable: Помечает поля, которые должны быть инъецированы. **/
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AutoInjectable {
 }
--- lab5/src/main/java/org/example/Injector.java
+++ lab5/src/main/java/org/example/Injector.java
@@ -1,4 +1,8 @@
 package org.example;
 
+import java.lang.reflect.Field;
+import java.util.Properties;
+
 public class Injector {
+
 }
